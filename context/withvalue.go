2a50e86a8e508c101256803ce9819bdc1dc2e594
henosteven <henosteven@126.com>
2018-05-25 21:02:17 +0800

    withvalue

--- context/withvalue.go
+++ context/withvalue.go
@@ -1,19 +1,19 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"time"
-	"context"
 )
 
-func watch(ctx context.Context, flag string) {
+func watch2(ctx context.Context) {
 	for {
 		select {
 		case <-ctx.Done():
-			fmt.Println("get~stop~signal" +  flag)
+			fmt.Println("get~stop~signal" +  ctx.Value("flag").(string))
 			return
 		default:
-			fmt.Println("just do it ~" + flag)
+			fmt.Println("just do it ~" + ctx.Value("flag").(string))
 		}
 		time.Sleep(time.Millisecond * 500)
 	}
@@ -24,10 +24,17 @@ func main() {
 	fmt.Println(ctx.Deadline()) //0001-01-01 00:00:00 +0000 UTC false
 	//false时表示没有设置截止时间，如果需要取消的话，需要调用取消函数进行取消。
 
-	go watch(ctx, "info1")
-	go watch(ctx, "info2")
-	go watch(ctx, "info3")
-	go watch(ctx, "info4")
+	ctxv := context.WithValue(ctx, "flag", "info1")
+	go watch2(ctxv)
+
+	ctxv = context.WithValue(ctx, "flag", "info2")
+	go watch2(ctxv)
+
+	ctxv = context.WithValue(ctx, "flag", "info3")
+	go watch2(ctxv)
+
+	ctxv = context.WithValue(ctx, "flag", "info4")
+	go watch2(ctxv)
 
 	time.Sleep(time.Second * 3)
 	cancel()
